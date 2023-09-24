eeaeadae10071716db8e5db2845ddda81881518b
Mehedi Nayem <naembfh@gmail.com>
2023-09-25 06:21:21 +0800

    change for host

--- programmingBuddy/settings.py
+++ programmingBuddy/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r22@z306@+3dj7=$)=_**p-fu^sj(1=m83quuk5q!(fc$a!itp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
